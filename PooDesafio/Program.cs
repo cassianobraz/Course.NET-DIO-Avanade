85b520a6e97a7f5b728ce5ab2cfac1d30d4f2315
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2025-09-05 15:42:54 -0400

    finish project POO

---
+++ PooDesafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 1100", "111111111111111", 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone");
+Smartphone iphone = new Iphone("987654321", "IPhone 13", "222222222222222", 128000);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
